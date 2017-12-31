7b4da07097cf256ee234536dc8c9e17c3fbf6230
Ruslan <ruslan.pedora.gr@gmail.com>
2017-12-31 21:02:47 +0200

    markers added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { 
+  NavController,
+  LoadingController } from 'ionic-angular';
+
 
 declare var google;
 
@@ -11,11 +14,18 @@ export class HomePage {
   private googleMapInjected = false;
   private mapInitialized = false;
   private map: any;
+  private marker: any = null;
+  private loading;
+  private markers: any[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public loadingCtrl: LoadingController) {
     window['googleMapCallback'] = () => {
       this.googleMapInjected = true;     
-      console.log('Google map injected'); 
+      this.loading.dismiss();      
+      this.initMap();      
+      console.log('Google map injected');
     }
     this.injectGoogleMapsSDK();
   }
@@ -26,10 +36,13 @@ export class HomePage {
 
     script.src = `http://maps.google.com/maps/api/js?key=${apiKey}&callback=googleMapCallback`
 
+    this.loading = this.loadingCtrl.create();
+    this.loading.present();
     document.body.appendChild(script);
   }
 
   initMap() {
+    
     this.map = new google.maps.Map(document.getElementById('map'), {
       center: new google.maps.LatLng(52.5167, 13.3833),
       zoom: 15,
@@ -37,6 +50,19 @@ export class HomePage {
     });
 
     this.mapInitialized = true;
+    this.createMarker();
   }
 
+  createMarker() {
+    this.markers.push(new google.maps.Marker({
+      position: this.map.getCenter(),
+      map: this.map,
+      draggable: true
+    }));  
+  }
+
+  clear() {
+    this.markers.forEach(el => el.setMap(null));
+    this.markers= [];
+  }
 }
